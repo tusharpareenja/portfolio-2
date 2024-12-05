4f90cda9f98d200fbd33666365515f6b15547c11
Tushar Pareenja <tusharpareenja@gmail.com>
2024-12-05 13:23:04 +0530

    Hi

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,29 +5,11 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const { scene } = useGLTF("./desktop_pc/scene.gltf");
-
-  // Function to sanitize the geometry by checking NaN values in the position attribute
-  const sanitizeGeometry = (mesh) => {
-    if (mesh && mesh.geometry && mesh.geometry.attributes.position) {
-      const positions = mesh.geometry.attributes.position.array;
-      for (let i = 0; i < positions.length; i++) {
-        if (isNaN(positions[i])) {
-          positions[i] = 0; // Set NaN values to 0 (or another default value)
-        }
-      }
-      mesh.geometry.attributes.position.needsUpdate = true;
-    }
-  };
-
-  // Sanitize geometry after the model is loaded
-  useEffect(() => {
-    sanitizeGeometry(scene);
-  }, [scene]);
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -38,7 +20,7 @@ const Computers = ({ isMobile }) => {
       />
       <pointLight intensity={1} />
       <primitive
-        object={scene}
+        object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
@@ -73,7 +55,7 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop="demand"
+      frameloop='demand'
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
